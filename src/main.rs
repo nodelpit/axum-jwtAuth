f41234712274c5d7ad40509b29f5da5452d8bc81
Noah Delpit <noahdelpit@protonmail.com>
2026-08-22 14:12:01 +0200

    ✨ Initialize Axum server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::net::TcpListener;
+mod routes;
+
+use crate::routes::app;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let listener = TcpListener::bind("127.0.0.1:8080").await?;
+
+    let addr = listener.local_addr().unwrap();
+    println!("Listening on {}", addr);
+
+    axum::serve(listener, app()).await?;
+
+    Ok(())
 }
